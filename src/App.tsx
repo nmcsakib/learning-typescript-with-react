6c657df58ab8a89d6ee5dd160466e3a8722208ce
nmcsakib <nmcsakib.1@gmail.com>
2025-06-03 20:25:17 +0600

    learned prop types for ts

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,31 @@ import User from "./components/User"
 
 function App() {
 
+  const users = [
+    {
+      id: 32423,
+      name: 'potato',
+      age: 14,
+      isReg: true
+    },
+    {
+      id: 323,
+      name: 'sakib',
+      age: 34,
+      isReg: false
+    },
+    {
+      id: 0o0000001,
+      name: 'esa sabela',
+      age: 34,
+      isReg: true
+    }
+  ]
+
   return (
     <div>
       <h1 className="text-red-500 text-6xl text-center">Hello World</h1>
-      <User number={23} id={'12203402'}/>
+      <User users={users}/>
     </div>
   )
 }
--- src/components/User.tsx
+++ src/components/User.tsx
@@ -1,7 +1,24 @@
-const User = (props: {number: number, id: string}) => {
+
+type UserProps = {
+    users: {
+        id: number;
+        name: string;
+        age: number;
+        isReg: boolean;
+    }[]
+
+}
+
+const User = ({ users }: UserProps) => {
     return (
         <div>
-          <h2 className="text-2xl text-center">The number is {props.number} and id is {props.id}</h2>  
+            {
+                users.map(user => <div className="text-rose-400 bg-cyan-300 border flex justify-center items-center gap-5 my-3 w-1/2 py-10">
+                    <h2 className="text-2xl text-center">The Name is {user.name} and id is {user.id}</h2>
+
+                    {user.isReg ? <p>Registerd</p> : <p>Not registerd</p>}
+                </div>)
+            }
         </div>
     );
 };
